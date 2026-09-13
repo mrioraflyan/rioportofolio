fb0c930f20e67d4c3411fda8f3899cf53642ed46
mrioraflyan <mrioraflyan23@gmail.com>
2026-09-13 20:17:43 +0700

    Add files via upload

---
+++ portofolio/porto.js
@@ -0,0 +1,80 @@
+alert("JavaScript berhasil terbaca");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const modal = document.getElementById("certificateModal");
+    const modalImage = document.getElementById("modalImage");
+
+    const closeModal = document.querySelector(".close-modal");
+    const buttons = document.querySelectorAll(".lihatsertifikat");
+
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+
+    let images = [];
+    let currentImage = 0;
+
+
+    buttons.forEach(function(button) {
+
+        button.addEventListener("click", function(e) {
+
+            e.preventDefault();
+
+            images = this.getAttribute("data-images").split(",");
+
+            currentImage = 0;
+
+            modalImage.src = images[currentImage];
+
+            modal.style.display = "flex";
+
+        });
+
+    });
+
+
+    nextBtn.addEventListener("click", function() {
+
+        currentImage++;
+
+        if (currentImage >= images.length) {
+            currentImage = 0;
+        }
+
+        modalImage.src = images[currentImage];
+
+    });
+
+
+    prevBtn.addEventListener("click", function() {
+
+        currentImage--;
+
+        if (currentImage < 0) {
+            currentImage = images.length - 1;
+        }
+
+        modalImage.src = images[currentImage];
+
+    });
+
+
+    closeModal.addEventListener("click", function() {
+
+        modal.style.display = "none";
+
+    });
+
+
+    modal.addEventListener("click", function(e) {
+
+        if (e.target === modal) {
+
+            modal.style.display = "none";
+
+        }
+
+    });
+
+});
